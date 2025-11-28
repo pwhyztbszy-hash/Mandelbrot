2805663ecae6a26e5fbd3e7f9df55f35a269069c
pwhyztbszy-hash <243625237+pwhyztbszy-hash@users.noreply.github.com>
2025-11-28 13:38:12 -0800

    Remove duplicate thread count logging
    Removed redundant thread count output from updateRender.

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -83,6 +83,9 @@ void ComplexPlane::loadText(Text& text)
     text.setString(ss.str());
 }
 
+int numThreads = thread::hardware_concurrency();
+cout << numThreads << " - number of threads";
+
 void ComplexPlane::updateRender()
 {
     int pixelWidth = m_pixel_size.x;
@@ -90,10 +93,7 @@ void ComplexPlane::updateRender()
 
     if (m_state == CALCULATING)
     {
-        int numThreads = thread::hardware_concurrency();
-        cout << numThreads << " - number of threads";
-        
-        
+           
         int rowsPerThread = pixelHeight / numThreads;
         vector<thread> threads;
         
